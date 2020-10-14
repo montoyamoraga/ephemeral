aaba5eee6d97dfe1e07c0d8bd65502f488435516
montoyamoraga <montoyamoraga@gmail.com>
2020-10-14 15:06:15 -0400

    add draft

--- script.py
+++ script.py
@@ -12,6 +12,7 @@ timeNow = datetime.datetime.now()
 # time delta to check files
 # measured in seconds
 timeDelta = 60
+timeDeltaDate = datetime.timedelta(seconds=timeDelta)
 
 def readTimeFile(pathFile):
   modificationTime = os.path.getmtime(pathFile)
@@ -22,14 +23,23 @@ def readTimeFolder(directoryFile):
     fullPath = os.path.join(directoryFile, fileName)
     modificationTime = readTimeFile(fullPath)
     # print(time.ctime(modificationTime))
-    modificationTime = datetime.datetime.fromtimestamp(modificationTime).strftime('%Y-%m-%d %H:%M:%S')
+    # modificationTime = datetime.datetime.fromtimestamp(modificationTime).strftime('%Y-%m-%d %H:%M:%S')
+    modificationTime = datetime.datetime.fromtimestamp(modificationTime)
+
     return modificationTime
 
 while True:
-  timeNow = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+  timeNow = datetime.datetime.now()
   print("timeNow:")
   print(timeNow)
   timeFile = readTimeFolder(myFolder)
   print("timeFile: ")
   print(timeFile)
+  print("difference:")
+  difference = timeNow - timeFile
+  print(difference)
+  if (difference > timeDeltaDate):
+    print("the file is deleted")
+  else:
+    print("the file is played")
   time.sleep(timeDelta)
