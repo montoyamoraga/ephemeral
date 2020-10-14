59f00c03c7a7bfa0af616b87786d57c235b1d54f
montoyamoraga <montoyamoraga@gmail.com>
2020-10-14 15:20:08 -0400

    add more functions

--- script.py
+++ script.py
@@ -3,43 +3,55 @@ import os
 import datetime
 import time
 
-# define directory to work with
+# define folder to work with
 myFolder = "myFolder/"
 
-# initialize current time
-timeNow = datetime.datetime.now()
-
-# time delta to check files
+# time interval to check files
 # measured in seconds
-timeDelta = 60
-timeDeltaDate = datetime.timedelta(seconds=timeDelta)
+timeInterval = 10
+timeIntervalDate = datetime.timedelta(seconds=timeInterval)
 
 def readTimeFile(pathFile):
   modificationTime = os.path.getmtime(pathFile)
   return modificationTime
 
 def readTimeFolder(directoryFile):
+  modificationTimes = []
   for fileName in os.listdir(directoryFile):
     fullPath = os.path.join(directoryFile, fileName)
     modificationTime = readTimeFile(fullPath)
-    # print(time.ctime(modificationTime))
-    # modificationTime = datetime.datetime.fromtimestamp(modificationTime).strftime('%Y-%m-%d %H:%M:%S')
     modificationTime = datetime.datetime.fromtimestamp(modificationTime)
+    modificationTimes.append(modificationTime)
+  # return array
+  return modificationTimes
 
-    return modificationTime
+def checkIfFilesExpire(modificationTimes, timeNow):
+  for index in range(len(modificationTimes)):
+    difference = timeNow - modificationTimes[index]
+
+    if (difference > timeIntervalDate):
+      print("this file should be played and deleted")
+    else:
+      print("this file should wait")
+
 
 while True:
+  # update current time
   timeNow = datetime.datetime.now()
-  print("timeNow:")
   print(timeNow)
-  timeFile = readTimeFolder(myFolder)
-  print("timeFile: ")
-  print(timeFile)
-  print("difference:")
-  difference = timeNow - timeFile
-  print(difference)
-  if (difference > timeDeltaDate):
-    print("the file is deleted")
-  else:
-    print("the file is played")
-  time.sleep(timeDelta)
+
+  # get array of times of modification of files
+  fileTimes = readTimeFolder(myFolder)
+
+  checkIfFilesExpire(fileTimes, timeNow)
+
+  # print("timeFile: ")
+  # print(timeFile)
+  # print("difference:")
+  # difference = timeNow - timeFile
+  # print(difference)
+  # if (difference > timeIntervalDate):
+  #   print("the file is deleted")
+  # else:
+  #   print("the file is played")
+  time.sleep(timeInterval)
