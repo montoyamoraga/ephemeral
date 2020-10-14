b0ca04bac8cefc96b803e2844248c1795a038a94
montoyamoraga <montoyamoraga@gmail.com>
2020-10-14 14:09:19 -0400

    add script.py

--- script.py
+++ script.py
@@ -1,1 +1,33 @@
-import numpy as np
+# import os
+import os
+import datetime
+import time
+
+# define directory to work with
+myDirectory = "myFiles/"
+
+# initialize current time
+timeNow = datetime.datetime.now()
+print(timeNow)
+
+# time delta to check files
+deltaTime = 4
+
+
+
+print("hello world")
+
+
+
+
+def readModificationTime(fullPath):
+  modifcationTime = os.path.getmtime(fullPath)
+  return modifcationTime
+
+
+for filename in os.listdir(myDirectory):
+  fullPath = os.path.join(myDirectory, filename)
+  modificationTime = readModificationTime(fullPath)
+  print(time.ctime(modificationTime))
+  modificationTime = datetime.datetime.fromtimestamp(modificationTime).strftime('%Y-%m-%d %H:%M:%S')
+  print(modificationTime)
